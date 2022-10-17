496029bafda0e6efe856d4112b4bd89ade5546e3
Alex Wade <alexjohnwade@gmail.com>
2022-10-17 14:24:42 +1100

    added the base files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
 import Favorites from './compponents/Favorites';
+import Meals from './compponents/Meals';
+import Modal from './compponents/Modal';
+import Search from './compponents/Search';
 
 function App() {
   return (
     <main>
-      <Favorites />
+      {/* <Search /> */}
+      {/* <Favorites /> */}
+      <Meals />
+      {/* <Modal /> */}
     </main>
   );
 }
