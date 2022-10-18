47302fa95cfefb67ea6fbf1c674b2aa349ca2354
Alex Wade <alexjohnwade@gmail.com>
2022-10-18 15:22:00 +1100

    added global conext

--- src/context.jsx
+++ src/context.jsx
@@ -3,9 +3,13 @@ import React, {useConext} from "react";
 const AppContext = React.createContext()
 
 const AppProvider = ({children}) => {
-  return <AppContext.Provider value = 'hello'>
+  return <AppContext.Provider value = {{name:'john', role:'student'}}>
     {children}
   </AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+  return useConext(AppContext)
 }
 
 export {AppContext, AppProvider}
