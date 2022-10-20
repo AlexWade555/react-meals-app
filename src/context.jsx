59d4dfaeffabd6dcd75e737d287867894cce47ee
Alex Wade <alexjohnwade@gmail.com>
2022-10-20 14:45:39 +1100

    added meals to iterate through

--- src/context.jsx
+++ src/context.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import axios from "axios";
 
 const AppContext = React.createContext()
@@ -7,12 +7,13 @@ const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 const AppProvider = ({children}) => {
+  const [meals, setMeals] = useState([])
 
   const fetchMeals = async (url) => {
     try {
-      const [data] = await axios(url)
+      const {data} = await axios(url)
+      setMeals(data.meals)
 
-      console.log(data)
     } catch (error) {
               console.log(error.response)
 
@@ -24,7 +25,7 @@ const AppProvider = ({children}) => {
     fetchMeals(allMealsUrl)
   },[])
 
-  return <AppContext.Provider value = {{name:'john', role:'student'}}>
+  return <AppContext.Provider value = {{meals}}>
     {children}
   </AppContext.Provider>
 }
