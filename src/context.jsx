8952928589476026718911313bdf7aefd4e43962
Alex Wade <alexjohnwade@gmail.com>
2022-11-16 14:00:57 +1100

    added    random meal context

--- src/context.jsx
+++ src/context.jsx
@@ -29,12 +29,16 @@ const AppProvider = ({children}) => {
     setLoading(false)
   }
 
+const fetchRandomMeal = () => {
+  fetchMeals(randomMealUrl)
+}
+
   useEffect(()=>{
     fetchMeals(`${allMealsUrl}${searchTerm}`)
 
   },[searchTerm])
 
-  return <AppContext.Provider value = {{loading, meals, setSearchTerm}}>
+  return <AppContext.Provider value = {{loading, meals, setSearchTerm, fetchRandomMeal}}>
     {children}
   </AppContext.Provider>
 }
