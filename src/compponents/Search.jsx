a7d2907837ee04f4aaea8b7653e1246e421d8cb1
Alex Wade <alexjohnwade@gmail.com>
2022-11-16 12:46:41 +1100

    added buttons to search component

--- src/compponents/Search.jsx
+++ src/compponents/Search.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react"
+import { useGlobalContext } from "../context"
+
 const Search = () => {
-  return <h1>Search Component</h1>
+  return <header className="search-container">
+    <form>
+      <input type="text" placeholder="type favourite meal" className="form-input" />
+      <button type="submit" className='btn btn-hipster'>search</button>
+      <button type="button" className='btn btn-hispter'>suprise me</button>
+    </form>
+  </header>
 }
 
 export default Search
