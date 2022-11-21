88469fbc588317b97934f513955ae9060afea93b
Alex Wade <alexjohnwade@gmail.com>
2022-11-21 11:56:51 +1100

    added close meal context and btn

--- src/compponents/Modal.jsx
+++ src/compponents/Modal.jsx
@@ -1,9 +1,13 @@
 import { useGlobalContext } from "../context"
 
 const Modal = () => {
+  const {selectedMeal, close} = useGlobalContext()
+
+
   return <aside className="modal-overlay">
     <div className="modal-container">
-      <h1>modal container</h1>
+      <h1>{selectedMeal.strMeal}</h1>
+      <button onClick={closeModal}>close</button>
     </div>
   </aside>
 }
